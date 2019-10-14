0c20a040749e57159dd212b614d6e7323eaca393
Jessy Declerck <declerck.jessy@gmail.com>
2019-10-14 11:27:08 +0200

    fix dto deserialization

--- src/main/java/com/pepito/i18napi/service/dto/NpcDto.java
+++ src/main/java/com/pepito/i18napi/service/dto/NpcDto.java
@@ -1,12 +1,16 @@
 package com.pepito.i18napi.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NpcDto {
 
     private Integer id;
--- src/main/java/com/pepito/i18napi/service/dto/PoiDto.java
+++ src/main/java/com/pepito/i18napi/service/dto/PoiDto.java
@@ -1,12 +1,16 @@
 package com.pepito.i18napi.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class PoiDto {
 
     private Integer id;
