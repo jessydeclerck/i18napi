78eace7a675da740ea4f4673feb687c46c9779d2
Jessy Declerck <declerck.jessy@gmail.com>
2019-10-14 11:19:54 +0200

    fix npc document

--- src/main/java/com/pepito/i18napi/db/document/Npc.java
+++ src/main/java/com/pepito/i18napi/db/document/Npc.java
@@ -2,9 +2,11 @@ package com.pepito.i18napi.db.document;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
+@Document
 public class Npc {
 
     private Integer npcId;
